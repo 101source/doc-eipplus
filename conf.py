67a47d4723df91de4e21566cef2758b9846d77c9
Edwin NG <edwin.ng@hundredplus.com>
2016-08-09 14:13:32 +0800

    Convert Worksheet to RST format

--- conf.py
+++ conf.py
@@ -273,8 +273,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'EIPPlusDocumentation.tex', u'EIP Plus Documentation Documentation',
-     u'Hundred Plus Corp. Ltd.', 'manual'),
+    (master_doc, 'EIPPlusDocumentation.tex', u'EIP Plus 線上說明',
+     u'百加資通 Hundred Plus Corp. Ltd.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -315,7 +315,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'eipplusdocumentation', u'EIP Plus Documentation Documentation',
+    (master_doc, 'eipplusdocumentation', u'EIP Plus 線上說明',
      [author], 1)
 ]
 
@@ -330,7 +330,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'EIPPlusDocumentation', u'EIP Plus Documentation Documentation',
+    (master_doc, 'EIPPlusDocumentation', u'EIP Plus 線上說明',
      author, 'EIPPlusDocumentation', 'One line description of project.',
      'Miscellaneous'),
 ]
