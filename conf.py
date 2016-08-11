02256439cfcadd5f14ac76f9241b155efbf13beb
張哲豪(Allen) <allen.chang@MAC108.local>
2016-08-11 09:58:47 +0800

    closes #1 add infolog help

--- conf.py
+++ conf.py
@@ -37,14 +37,14 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 
-from recommonmark.parser import CommonMarkParser
+#from recommonmark.parser import CommonMarkParser
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+#source_parsers = {
+#    '.md': CommonMarkParser,
+#}
 #
-source_suffix = ['.rst', '.md']
-#source_suffix = '.rst'
+#source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The encoding of source files.
 #
